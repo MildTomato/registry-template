fbaf7ce0e633e01318ceb81f85484d742b3115f7
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2025-01-18 16:49:43 +0800

    update name

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,15 @@ export default function Home() {
       </header>
       <main className="flex flex-col flex-1 gap-8">
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <Command name={`auth-login-01`} />
+          <Command name={`auth-login-01-nextjs-app-router`} />
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
               A login form with Supabase authentication.
             </h2>
-            <OpenInV0Button name="auth-login-01" className="w-fit" />
+            <OpenInV0Button
+              name="auth-login-01-nextjs-app-router"
+              className="w-fit"
+            />
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <AuthLogin01 />
